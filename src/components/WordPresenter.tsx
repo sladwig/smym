d9a11f556670d5655419c8e9b3a3be3483c58b63
Stefan Ladwig <koenig.s@gmail.com>
2019-12-28 16:28:46 +0100

    refactor: interpolation dry up

--- src/components/WordPresenter.tsx
+++ src/components/WordPresenter.tsx
@@ -168,36 +168,19 @@ const PaidTokenDisplay = ({ word }: TD<paidToken>) => {
         x: state,
         config: { ...config.stiff, clamp: true },
     });
+    const to = useCallback((output: number[]) => ({ range: [1, 4], output }), []);
 
     return (
         <animated.div
             className="paid-token"
             style={{
-                opacity: x.interpolate({ range: [1, 4], output: [0.3, 1] }),
-                borderRadius: x.interpolate({
-                    range: [1, 4],
-                    output: [0, 32],
-                }),
-                fontSize: x.interpolate({
-                    range: [1, 4],
-                    output: [36, 24],
-                }),
-                paddingTop: x.interpolate({
-                    range: [1, 4],
-                    output: [0, 14],
-                }),
-                paddingBottom: x.interpolate({
-                    range: [1, 4],
-                    output: [0, 14],
-                }),
-                paddingLeft: x.interpolate({
-                    range: [1, 4],
-                    output: [0, 19],
-                }),
-                paddingRight: x.interpolate({
-                    range: [1, 4],
-                    output: [0, 19],
-                }),
+                opacity: x.interpolate(to([0.3, 1])),
+                borderRadius: x.interpolate(to([0, 32])),
+                fontSize: x.interpolate(to([36, 24])),
+                paddingTop: x.interpolate(to([0, 14])),
+                paddingBottom: x.interpolate(to([0, 14])),
+                paddingLeft: x.interpolate(to([0, 19])),
+                paddingRight: x.interpolate(to([0, 19])),
             }}
         >
             <>{word.characters.map(display)}</>
