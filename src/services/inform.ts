c1b72d1821ca7a1afcd0bbbd7b38693d650f8144
Stefan Ladwig <koenig.s@gmail.com>
2019-12-31 19:15:14 +0100

    feature: updated inform text

--- src/services/inform.ts
+++ src/services/inform.ts
@@ -5,9 +5,9 @@ export const inform = (apiToken: string, user: IUser, shouldSlack: boolean) => {
     let message = '';
     if (user.transactions.length > 0) {
         const lastTransaction = user.transactions.slice(-1).pop()!;
-        message = `added '${lastTransaction.description}' ${lastTransaction.value.toFixed(
+        message = `added '${lastTransaction.description} ${lastTransaction.value.toFixed(
             2,
-        )} to your balance - new total: ${user.balance.toFixed(2)}`;
+        )}' to your balance - new total: ${user.balance.toFixed(2)}`;
     } else {
         message = `Paid up! - Your balance is ${user.balance.toFixed(2)}`;
     }
